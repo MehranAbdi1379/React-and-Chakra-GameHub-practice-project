e160ad52f7f8b2b70463bd8fb5598c038a031c2c
MehranAbdi1379 <mehranabdi13793@gmail.com>
2023-04-25 09:29:46 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+      <Dashboard></Dashboard>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Flex, Icon, Image } from "@chakra-ui/react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <Flex>
+      <Image></Image>
+    </Flex>
+  );
+};
+
+export default Navbar;
